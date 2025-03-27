2348cfb6d8a472baad5b94ec887e2ba2b19b449f
Anders Simensen <anders@Anderss-MacBook-Pro.local>
2025-03-27 20:46:21 +0100

    added compose and workdir for app

---
+++ server/backend/app/app.py
@@ -0,0 +1,6 @@
+# Håndtere QR-kode-scanning
+# Blokkere om reserver/aktiv
+
+
+# Fjerne/legge til scootere til tilgjengelige scootere liste
+# Blokkere reservering av en scooter som allerede er reservert/aktiv
